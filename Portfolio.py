827c80fc293daefd63abe9395f953ec487a57308
ekhaikhina <j.khaikhina@gmail.com>
2019-07-02 03:30:29 -0400

    added portfolio tracking functionality, fixed missing dates in fx

--- Portfolio.py
+++ Portfolio.py
@@ -7,28 +7,39 @@ class Portfolio:
     """ the portfolio with with weights and assets with other operations
     """
 
-    def __init__(self, instrumentsNWeights, totalInvest):
+    def __init__(self, instrumentsNAmounts, totalInvest, setup_date):
         """
         Initialize the portfolio with tuple of instruments and the corresponding
-        weights.
+        amounts held.
 
         public attribute
-        :param instrumentsNWeights: dict[str] -> float
+        :param instrumentsNAmounts: dict[str] -> float
         :param totalInvest: float
         total amount of USD used in this portfolio investment
         """
         # add all necessary attributes here which link to portfolio and
         # will be tracked.
-        self.portfolio = instrumentsNWeights
+        self.portfolio = instrumentsNAmounts
         self.totalInvest = totalInvest
+        self.all_portfolios={setup_date:self}
 
     def getPortfolioValue(self, t):
         """
         Return the portfolio value at time t
         :return: float
         """
-        pass
-
+        
+        my_key=[i for i in self.all_portfolios.keys() if i <= t][-1]
+        my_portfolio=self.all_portfolios[my_key]
+        
+        sum_tmp=0
+        for item in my_portfolio.portfolio.keys():
+            price=universe.get_price_in_currency(item,t,'CAD')
+            #price=universe.get_security(item).price[t]
+            amount=my_portfolio.portfolio[item]
+            sum_tmp=sum_tmp+price*amount
+        
+        return sum_tmp
 
     def addPortfolio(self, other):
         """
@@ -36,6 +47,7 @@ class Portfolio:
         :param other: Portofolio
         :return: Portfolio
         """
-        pass
+        self.portfolio=other.portfolio
+        self.all_portfolios[list(other.all_portfolios.keys())[0]]=other
 
     # we may also need some methods to do portfolio level risk analysis
